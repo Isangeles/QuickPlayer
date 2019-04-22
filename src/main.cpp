e56f0d431f39b885c26c6bc51229e91bb62d8273
Isangeles <darekapo1@gmail.com>
2019-04-22 19:46:03 +0200

    GPLv2 license, readme update

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    //TEST CODE
-    MusicPlayer *quickPlayer = new MusicPlayer();
-    //quickPlayer->next();
-    std::cout << (quickPlayer->playlist()->currentMedia().canonicalUrl().toString().toStdString());
-    //quickPlayer->play();
-    //
     return a.exec();
 }
